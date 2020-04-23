8020535223ad33bf0d5529ed2f67a7230daa379b
alexei <webdesfreelance@gmail.com>
2020-04-23 15:54:41 +0300

    First save

---
+++ script.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+let availableQuantity=15;
+let  orderedQuantity=prompt('Please Input Order Value');
+
+
+
+if(orderedQuantity>availableQuantity)
+{console.log('Sorry, we don’t have enough in stock');}
+else
+{console.log('Great, we will soon ship your order');}
+
+
+
---
+++ script2.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+let availablQuantity = 15;
+let  ordereQuantity = prompt('Please Input Order Value');
+let result = (ordereQuantity>availablQuantity)?'Sorry, we don’t have enough in stock':'Great, we will soon ship your order';
+console.log(result);
